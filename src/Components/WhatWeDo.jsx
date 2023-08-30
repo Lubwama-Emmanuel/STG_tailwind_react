64d63ef2ee8f6af657c32d5a410c563614d35a02
Rex <lubwamaemmanuel1@gmail.com>
2023-08-30 18:55:35 +0300

    feat:(What we do component)

--- src/Components/WhatWeDo.jsx
+++ src/Components/WhatWeDo.jsx
@@ -1,3 +1,7 @@
 export default function WhatWeDo() {
-  return <div></div>;
+  return (
+    <div className=" mb-4 bg-gradient-to-r from-amber-950 to-amber-400 bg-clip-text text-center text-3xl font-semibold text-transparent">
+      <h2>Curious? This is what we do</h2>
+    </div>
+  );
 }
