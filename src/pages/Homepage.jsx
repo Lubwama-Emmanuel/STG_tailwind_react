262be2a2f9545d9c9878d48dbbd49dd19d35dc2e
Rex <lubwamaemmanuel1@gmail.com>
2023-08-30 18:56:37 +0300

    refactor:(a little clean up)

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,14 @@
 import Hero from "../Components/Hero";
+import Vision from "../Components/Vision";
 import WhatWeDo from "../Components/WhatWeDo";
+import WhySimba from "../Components/WhySimba";
 
 export default function Homepage() {
   return (
     <div>
       <Hero />
+      <Vision />
+      <WhySimba />
       <WhatWeDo />
     </div>
   );
