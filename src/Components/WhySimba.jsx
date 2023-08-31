f71837f19e91e188838ef5dd7a963595284bb55a
Rex <lubwamaemmanuel1@gmail.com>
2023-08-31 18:19:47 +0300

    fix:(little clean up)

--- src/Components/WhySimba.jsx
+++ src/Components/WhySimba.jsx
@@ -6,7 +6,7 @@ export default function WhySimba() {
       }}
       className="m-auto mb-20 bg-stone-100 pt-28"
     >
-      <h2 className=" mb-2 bg-gradient-to-r from-amber-400 to-amber-800 bg-clip-text text-center text-3xl font-semibold text-transparent">
+      <h2 className="mb-2 bg-gradient-to-r from-amber-400 to-amber-800 bg-clip-text text-center text-3xl font-semibold text-transparent">
         Why Simba Talents?
       </h2>
       <div className="m-auto grid max-w-6xl grid-cols-3 gap-16 py-10">
@@ -60,8 +60,8 @@ export default function WhySimba() {
 function Reason({ title, svg, desc }) {
   return (
     <div className="space-y-4">
-      <div className="">
-        <div className="">
+      <div>
+        <div>
           <img src={`${svg}.svg`} alt="africa" className="h-20" />
         </div>
         <h3 className="bg-clip-text font-semibold uppercase hover:bg-gradient-to-r hover:from-amber-400 hover:to-amber-800 hover:text-transparent">
