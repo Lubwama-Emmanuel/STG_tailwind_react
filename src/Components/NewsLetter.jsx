3da44781af6b2a8238b7cd9880a240ae5a6ebf3c
Rex <lubwamaemmanuel1@gmail.com>
2023-08-31 18:14:02 +0300

    feat:(newsletter component)

---
+++ src/Components/NewsLetter.jsx
@@ -0,0 +1,10 @@
+export default function NewsLetter() {
+  return (
+    <section
+      style={{
+        backgroundImage: `url(Email.png)`,
+      }}
+      className="mb-20 h-48 bg-gradient-to-b  from-amber-100  to-stone-100 bg-contain bg-right bg-no-repeat"
+    ></section>
+  );
+}
