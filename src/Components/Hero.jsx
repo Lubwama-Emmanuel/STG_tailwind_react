a3c8938aca6b2b719b4655316cfdd1180566e74f
Rex <lubwamaemmanuel1@gmail.com>
2023-09-01 07:48:42 +0300

    feat:(added mobile responsiveness)

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,14 +6,14 @@ import Button from "../ui/Button";
 
 export default function Hero() {
   return (
-    <div
+    <section
       style={{
         clipPath: "polygon(0 0%, 100% 0%, 100% 85%, 0% 100%)",
       }}
-      className="w-full bg-gradient-to-b  from-amber-100  to-stone-100"
+      className="w-full bg-gradient-to-b  from-amber-100  to-stone-100 pb-5 sm:pb-10"
     >
       <div className="m-auto py-10 text-center sm:w-3/4 sm:py-20">
-        <h2 className="  mb-10 mt-20 text-2xl font-bold uppercase sm:text-6xl">
+        <h2 className="mb-5 text-2xl font-bold uppercase sm:mb-10 sm:mt-20 sm:text-6xl">
           looking to{" "}
           <span className="bg-gradient-to-r from-amber-400 to-amber-800 bg-clip-text text-transparent">
             tap{" "}
@@ -24,7 +24,7 @@ export default function Hero() {
           </span>
           pool?
         </h2>
-        <p className="m-auto sm:mb-16 sm:w-3/4">
+        <p className="m-auto mb-5 sm:mb-16 sm:w-3/4">
           Simba Talents is comprised of the top graduates of the finest
           universities in Africa are carefully selected through a rigorous
           hiring process. They are highly skilled and trained to provide quality
@@ -34,7 +34,7 @@ export default function Hero() {
           <Button>Lets talk</Button>
         </div>
 
-        <h3 className="mb-4 mt-20">Trusted by companies like</h3>
+        <h3 className="mb-2 sm:mb-4 sm:mt-20">Trusted by companies like</h3>
         <div className="flex items-center justify-center space-x-4 text-center">
           <div className="w-10 sm:w-20">
             <img src="/companies/marsbased-logo.png" alt="company 1" />
@@ -50,6 +50,6 @@ export default function Hero() {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
