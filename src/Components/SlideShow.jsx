9583047fad98634161dae348f3678966c3e15a52
Rex <lubwamaemmanuel1@gmail.com>
2023-09-01 11:09:45 +0300

    feat:(added more langs)

--- src/Components/SlideShow.jsx
+++ src/Components/SlideShow.jsx
@@ -6,7 +6,7 @@ export default function SlideShow() {
       <h2 className="mb-10 bg-gradient-to-r from-amber-950 to-amber-400 bg-clip-text text-center text-3xl font-semibold text-transparent">
         Technologies used
       </h2>
-      <Slide autoplay slidesToShow={4} arrows={false} duration={1000}>
+      <Slide autoplay slidesToShow={5} arrows={false} duration={1000}>
         <div className="each-slide-effect">
           <svg className="h-14 sm:h-28">
             <use xlinkHref="sprite.svg#icon-javascript"></use>
@@ -25,6 +25,21 @@ export default function SlideShow() {
         <div className="each-slide-effect">
           <svg className="h-14 sm:h-28">
             <use xlinkHref="sprite.svg#icon-amazonaws"></use>
+          </svg>
+        </div>
+        <div className="each-slide-effect">
+          <svg className="h-14 sm:h-28">
+            <use xlinkHref="sprite.svg#icon-react"></use>
+          </svg>
+        </div>
+        <div className="each-slide-effect">
+          <svg className="h-14 sm:h-28">
+            <use xlinkHref="sprite.svg#icon-node-dot-js"></use>
+          </svg>
+        </div>
+        <div className="each-slide-effect">
+          <svg className="h-14 sm:h-28">
+            <use xlinkHref="sprite.svg#icon-javascript"></use>
           </svg>
         </div>
       </Slide>
