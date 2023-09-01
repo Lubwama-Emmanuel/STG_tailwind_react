8c82563a734ad672be37e902e3e2cb337829a929
Rex <lubwamaemmanuel1@gmail.com>
2023-09-01 08:30:37 +0300

    feat:(added mobile responsiveness)

--- src/Components/Vision.jsx
+++ src/Components/Vision.jsx
@@ -1,11 +1,11 @@
 export default function Vision() {
   return (
-    <div className="sm:py-15 m-auto grid w-4/5 grid-cols-[2fr_1fr] gap-10 py-20">
+    <section className="sm:py-15 m-auto w-4/5 sm:grid sm:grid-cols-[2fr_1fr] sm:gap-10">
       <div>
         <h3 className="mb-4 bg-gradient-to-r from-amber-400 to-amber-800 bg-clip-text text-3xl font-semibold text-transparent">
           Our vision
         </h3>
-        <p className="text-justify text-lg">
+        <p className="pb-5 text-justify text-lg">
           At Simba Talents, we have a unique mission – to empower the unemployed
           and underemployed in Africa by giving them opportunities to showcase
           their skills. Our team is made up of exceptional graduates from top
@@ -18,6 +18,6 @@ export default function Vision() {
       <div>
         <img src="vision.svg" alt="vision" />
       </div>
-    </div>
+    </section>
   );
 }
